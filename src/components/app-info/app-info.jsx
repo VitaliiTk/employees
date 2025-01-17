57bca7789015b8f3794893de2d48c13cdcb05931
VitaliiTk <tka4uk.gt41@gmail.com>
2025-01-17 15:04:37 +0600

    add bootstrap

---
+++ src/components/app-info/app-info.jsx
@@ -0,0 +1,9 @@
+export default function AppInfo() {
+  return (
+    <div className='ap-info'>
+      <h1>Учет сотрудников в компании N</h1>
+      <h2>Общее количество сотрудников: </h2>
+      <h2>Премию получат: </h2>
+    </div>
+  )
+}
---
+++ src/components/app/app.jsx
@@ -0,0 +1,11 @@
+import './app.css'
+
+import AppInfo from '../app-info/app-info'
+
+export default function App() {
+  return (
+    <div className='app'>
+      <AppInfo />
+    </div>
+  )
+}

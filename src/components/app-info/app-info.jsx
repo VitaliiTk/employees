bce4af5f11dc5a98a1f1991a15e754be0eb50363
VitaliiTk <tka4uk.gt41@gmail.com>
2025-01-17 15:07:51 +0600

    stylize app-info component

--- src/components/app-info/app-info.jsx
+++ src/components/app-info/app-info.jsx
@@ -1,6 +1,8 @@
+import './app-info.css'
+
 export default function AppInfo() {
   return (
-    <div className='ap-info'>
+    <div className='app-info'>
       <h1>Учет сотрудников в компании N</h1>
       <h2>Общее количество сотрудников: </h2>
       <h2>Премию получат: </h2>

ac3a0de51a180e51db706612da8bb980ed42a5cd
VitaliiTk <tka4uk.gt41@gmail.com>
2025-01-17 15:21:09 +0600

    finish app-filter comp

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,18 @@
 import './app.css'
 
 import AppInfo from '../app-info/app-info'
+import SearchPanel from '../search-panel/search-panel'
+import AppFilter from '../app-filter/app-filter'
 
 export default function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <AppInfo />
+
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
     </div>
   )
 }
